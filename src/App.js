206e2eafa8bd76b7dceee2b6da42e7d9b114e032
Alex <alexrabin@me.com>
2021-08-06 13:16:57 -0700

    added react memes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      imageURL: null
+    };
+  }
+    render(){
+      const { error, isLoaded, imageURL } = this.state;
+      
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h2>React Memes</h2>
+            {error && <div>Error: {error.message}</div>}
+            {!isLoaded && <div>Loading...</div>}
+            {imageURL && <img src={imageURL} alt='meme'/>}
+            <button onClick={this.getNewMeme.bind(this)}>New Meme</button>
+          </header>
+        </div>
+      );
+    }
+    componentDidMount(){
+      this.getNewMeme();
+    }
+    // shouldComponentUpdate(prevProps){
+    //   return prevProps.isLoaded !== this.props.isLoaded
+    // }
+    getNewMeme(){
+      if (this.state.isLoaded){
+        this.setState({
+          isLoaded: false,
+        });
+      }
+      fetch(`https://meme-api.herokuapp.com/gimme/wholesomememes`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            imageURL: result.url
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    }
 }
 
 export default App;
